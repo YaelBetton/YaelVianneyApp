3ae03fe198686b6443c090efc01f5d4533d3d147
betton <yael.betton@etu.unilim.fr>
2026-03-04 15:08:49 +0100

    Initial Commit

---
+++ YaelApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace YaelApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
