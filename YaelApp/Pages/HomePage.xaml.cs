c7102d685bf9ffacc1a0cd0e337a11c3e121cd10
betton <yael.betton@etu.unilim.fr>
2026-03-10 11:54:03 +0100

    Add image carousel with arrow

--- YaelApp/Pages/HomePage.xaml.cs
+++ YaelApp/Pages/HomePage.xaml.cs
@@ -6,5 +6,26 @@ public partial class HomePage : ContentPage
     {
         InitializeComponent();
     }
-}
 
+    private void OnPreviousClicked(object? sender, EventArgs e)
+    {
+        if (NewsCarousel.ItemsSource is not System.Collections.IList items || items.Count == 0)
+        {
+            return;
+        }
+
+        var previous = NewsCarousel.Position - 1;
+        NewsCarousel.Position = previous < 0 ? items.Count - 1 : previous;
+    }
+
+    private void OnNextClicked(object? sender, EventArgs e)
+    {
+        if (NewsCarousel.ItemsSource is not System.Collections.IList items || items.Count == 0)
+        {
+            return;
+        }
+
+        var next = NewsCarousel.Position + 1;
+        NewsCarousel.Position = next >= items.Count ? 0 : next;
+    }
+}
